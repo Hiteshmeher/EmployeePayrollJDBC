5a29a5a6f4624d8fde6599502c863abd0a5e8f4a
Hitesh Kumar Meher <107098715+Hiteshmeher@users.noreply.github.com>
2022-09-19 10:42:54 +0530

    Created payroll service database and java program to connect to database

--- EmployeePayrollJDBC/src/main/java/com/bridgelabz/EmpPayroll.java
+++ EmployeePayrollJDBC/src/main/java/com/bridgelabz/EmpPayroll.java
@@ -3,18 +3,18 @@ package com.bridgelabz;
 public class EmpPayroll {
     private int id;
     private String name;
-    private double salary;
+    private String address;
 
     /**
      * Constructors
      * @param id
      * @param name
-     * @param salary
+     * @param address
      */
-    public EmpPayroll(int id, String name, double salary){
+    public EmpPayroll(int id, String name, String address){
         this.id = id;
         this.name = name;
-        this.salary = salary;
+        this.address = address;
     }
     // toString methods
     @Override
@@ -22,7 +22,7 @@ public class EmpPayroll {
         return "EmployeePayroll{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
-                ", salary=" + salary +
+                ", salary=" + address +
                 '}';
     }
 }
